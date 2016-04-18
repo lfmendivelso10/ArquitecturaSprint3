c8dc2f72fbeccf63ef8c230de9487dafd08a9a98
Luis Felipe Mendivelso Osorio <lf.mendivelso10@uniandes.edu.co>
2016-04-18 12:14:36 -0500

    ABCMasctoas beta-final2

--- config/unircorn.rb
+++ config/unircorn.rb
@@ -23,21 +23,15 @@ stdout_path "#{app_dir}/log/unicorn.stdout.log"
 # Set master PID location
 pid "#{shared_dir}/pids/unicorn.pid"
 
+before_fork do |server, worker|
+  @sidekiq_pid ||= spawn('bundle exec sidekiq -e production -c config/sidekiq.yml')
+end
+
 after_fork do |server, worker|
-
-# In-Memory datastore
-  redis_conn = proc {
-    Redis.new
-  }
-# Server configuration por 102
-  Sidekiq.configure_server do |config|
-    config.redis = {url: 'redis://localhost:6379/12'}
-    config.redis = ConnectionPool.new(size: 102, &redis_conn)
-
-  end
-# Client Configuration
   Sidekiq.configure_client do |config|
-    config.redis = {url: 'redis://localhost:6379/12'}
-    config.redis = ConnectionPool.new(size: 102, &redis_conn)
+    config.redis = { :size => 100 }
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 100 }
   end
 end
