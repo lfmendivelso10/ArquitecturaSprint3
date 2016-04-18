799a4abbcef55fee417a891855eb081e2563dff2
Luis Felipe Mendivelso Osorio <lf.mendivelso10@uniandes.edu.co>
2016-04-18 11:44:32 -0500

    ABCMasctoas beta-final1

--- config/unircorn.rb
+++ config/unircorn.rb
@@ -1,4 +1,8 @@
 # set path to application
+# Sidekiq Global Configuration
+#
+require 'sidekiq'
+
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/tmp"
 working_directory app_dir
@@ -18,3 +22,22 @@ stdout_path "#{app_dir}/log/unicorn.stdout.log"
 
 # Set master PID location
 pid "#{shared_dir}/pids/unicorn.pid"
+
+after_fork do |server, worker|
+
+# In-Memory datastore
+  redis_conn = proc {
+    Redis.new
+  }
+# Server configuration por 102
+  Sidekiq.configure_server do |config|
+    config.redis = {url: 'redis://localhost:6379/12'}
+    config.redis = ConnectionPool.new(size: 102, &redis_conn)
+
+  end
+# Client Configuration
+  Sidekiq.configure_client do |config|
+    config.redis = {url: 'redis://localhost:6379/12'}
+    config.redis = ConnectionPool.new(size: 102, &redis_conn)
+  end
+end
